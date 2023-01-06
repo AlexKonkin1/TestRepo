fe44c5f248f3cd0ab501f19116df9fd67c74aa1b
AlexKonkin1 <122062422+AlexKonkin1@users.noreply.github.com>
2023-01-06 04:38:34 +0300

    Create ForwardCommand.cs

---
+++ RobotClassLibrary/ForwardCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotClassLibrary
+{
+    public class ForwardCommand : AbstractсCommand
+    {
+        public override (int outX, int outY, PositionOrientedObject.OrientationEnum outOrientation) Execute(int x, int y, PositionOrientedObject.OrientationEnum orientationIn)
+        {
+            var outX = x;
+            var outY = y;
+            var outOrientation = orientationIn;
+            switch (orientationIn)
+            {
+                case PositionOrientedObject.OrientationEnum.N:
+                    outY++;
+                    break;
+                case PositionOrientedObject.OrientationEnum.E:
+                    outX++;
+                    break;
+                case PositionOrientedObject.OrientationEnum.S:
+                    outY--;
+                    break;
+                case PositionOrientedObject.OrientationEnum.W:
+                    outX--;
+                    break;
+                default:
+                    break;
+            }
+            return (outX, outY, outOrientation);
+        }
+    }
+}
