3cb73ede183b71d1b3c73ced0dbbc9c471409423
AlexKonkin1 <122062422+AlexKonkin1@users.noreply.github.com>
2023-01-06 04:38:30 +0300

    Create AbstractсCommand.cs

---
+++ RobotClassLibrary/AbstractсCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotClassLibrary
+{
+    public abstract class AbstractсCommand
+    {
+        public abstract (int outX,  int outY,  PositionOrientedObject.OrientationEnum outOrientation) Execute(int x, int y, PositionOrientedObject.OrientationEnum orientationIn);
+
+    }
+}
