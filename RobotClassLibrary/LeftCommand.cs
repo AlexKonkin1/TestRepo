f56dba90ac4d68fdaa541c0ac49281a21b83683b
AlexKonkin1 <122062422+AlexKonkin1@users.noreply.github.com>
2023-01-06 04:38:37 +0300

    Create LeftCommand.cs

---
+++ RobotClassLibrary/LeftCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotClassLibrary
+{
+    public class LeftCommand : AbstractсCommand
+    {
+        public override (int outX, int outY, PositionOrientedObject.OrientationEnum outOrientation) Execute(int x, int y, PositionOrientedObject.OrientationEnum orientationIn)
+        {
+            var outX = x;
+            var outY = y;
+            var outOrientation = orientationIn;
+            switch (orientationIn)
+            {
+                case PositionOrientedObject.OrientationEnum.N:
+                    outOrientation = PositionOrientedObject.OrientationEnum.W;
+                    break;
+                case PositionOrientedObject.OrientationEnum.W:
+                    outOrientation = PositionOrientedObject.OrientationEnum.S;
+                    break;
+                case PositionOrientedObject.OrientationEnum.S:
+                    outOrientation = PositionOrientedObject.OrientationEnum.E;
+                    break;
+                case PositionOrientedObject.OrientationEnum.E:
+                    outOrientation = PositionOrientedObject.OrientationEnum.N;
+                    break;
+                default:
+                    break;
+            }
+            return (outX, outY, outOrientation);
+        }
+    }
+}
